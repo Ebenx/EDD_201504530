1f346cac9f0c56d493c58c7c495b034c9b9d5a40
Ebenx <ebenja42@gmail.com>
2017-12-23 23:51:48 -0600

    Tarea 3 abb

---
+++ Tarea 3/TareaAbolBB/TareaAbolBB/ABB.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaAbolBB
+{
+    class ABB
+    {
+        public NodoABB raiz;
+        public ABB()
+        {
+            raiz = null;
+        }
+
+        public void insertar(int id)
+        {
+            NodoABB nuevo = new NodoABB(id);
+
+            if (raiz==null)
+            {
+                raiz = nuevo;
+            }else
+            {
+                inser(raiz, nuevo);
+            }
+        }
+
+        public void inser(NodoABB raiz, NodoABB nuevo)
+        {
+            if (nuevo.id > raiz.id)
+            {
+                if (raiz.der == null)
+                {
+                    raiz.der = nuevo;
+                }else
+                {
+                    inser(raiz.der,nuevo);
+                }
+            }else if (nuevo.id<raiz.id)
+            {
+                if (raiz.izq == null)
+                {
+                    raiz.izq = nuevo;
+                }
+                else
+                {
+                    inser(raiz.izq, nuevo);
+                }
+            }
+        }
+
+        public string preOrden(NodoABB nodo)
+        {
+            if (nodo != null)
+            {
+                return ","+nodo.id.ToString() + "" + preOrden(nodo.izq) +""+ preOrden(nodo.der);
+            }else
+            {
+                return "";
+            }      
+        }
+
+        public string inOrden(NodoABB nodo)
+        {
+            if (nodo != null)
+            {
+                return inOrden(nodo.izq) + ","+ nodo.id.ToString() + inOrden(nodo.der);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public string postOrden(NodoABB nodo)
+        {
+            if (nodo != null)
+            {
+                return  postOrden(nodo.izq) + "" + postOrden(nodo.der)+ ","+nodo.id.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+    }
+}
---
+++ Tarea 3/TareaAbolBB/TareaAbolBB/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TareaAbolBB
+{
+    public partial class Form1 : Form
+    {
+        ABB arbol = new ABB();
+        NodoABB nodo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            arbol.insertar(Int32.Parse(textBox1.Text));
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "PreORden" + arbol.preOrden(arbol.raiz)+ "\nInOrden" + arbol.inOrden(arbol.raiz)+
+                "\nPostOrden" + arbol.postOrden(arbol.raiz);
+        }
+
+     
+    }
+}
